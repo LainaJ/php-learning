a97bfdec9696236ff13695effd08a47b406cb859
LainaJ <lainakarosic@gmail.com>
2020-03-03 17:23:28 -0500

    contatenation interpolation php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Hello World";
+$greeting = "Hey there, ";
+
+$name = "Laina";
+
+//one way to interpolate 
+echo "Hello, $name";
+
+//use period space to concatenate
+echo 'Hey! I am using a period to concatenate, ' . $name;
+
+//above, regular interpolation uses just $
+// alternatively, can use or {$}
+echo "How are you doing with these interpolation things, {$name}?";
+
+
